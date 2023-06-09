b2d0ce9eb543ace749c90e997d27a8a47921c871
Alpari0 <128226964+Alpari0@users.noreply.github.com>
2023-06-09 17:18:56 +0200

    Add files via upload
    Ćwiczenia 2-4

---
+++ Ćwiczenie2_Obwód_VS/Ćwiczenie2_Obwód_VS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Skrobol Adrian
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double obwód, pole, promień, liczbaPi;
+        liczbaPi = 3.141592;
+        Console.WriteLine("Podaj pole koła aby obliczyć obwód");
+        pole = inputValue();
+        promień = Math.Sqrt(pole / liczbaPi);
+        obwód = 2 * liczbaPi * Math.Sqrt(pole / liczbaPi);
+        Console.WriteLine("Obwód koła o polu: " + pole + " wynosi: " + obwód);
+    }
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
